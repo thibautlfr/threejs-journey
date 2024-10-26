a8f946af5018838bb409610beb8ad3d5f83b555d
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-10-27 00:43:52 +0200

    [resize/pixel-ratio]:07-fullscreen-and-resizing

--- src/script.js
+++ src/script.js
@@ -26,6 +26,20 @@ const sizes = {
     height: window.innerHeight
 }
 
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
 /**
  * Camera
  */
@@ -45,6 +59,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
