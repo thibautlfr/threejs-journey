7c6521fa3713582a9c263263135cab6086641d44
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-10-27 00:04:56 +0200

    [starter]:07-fullscreen-and-resizing

--- src/script.js
+++ src/script.js
@@ -1,17 +1,5 @@
 import * as THREE from 'three'
-import {OrbitControls} from "three/addons";
-
-/**
- * Cursors
- */
-const cursor = {
-    x: 0,
-    y: 0
-}
-window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5
-    cursor.y = - (event.clientY / sizes.height - 0.5)
-})
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
@@ -19,77 +7,53 @@ window.addEventListener('mousemove', (event) => {
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600,
-    aspectRatio: 800 / 600
-}
-
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Camera
-const camera = new THREE.PerspectiveCamera(
-    75, // field of view : between 0 and 180 but in general 45 to 75
-    sizes.width / sizes.height, // aspect ratio
-    0.1, // near clipping plane : the minimum distance that the camera can see
-    100 // far clipping plane : the maximum distance that the camera can see
-    // if the object is outside of this range, it will not be rendered
-)
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
 
-// const camera = new THREE.OrthographicCamera(
-//     -1 * sizes.aspectRatio, // the left edge of the camera
-//     1 * sizes.aspectRatio, // right
-//     1, // top
-//     -1, // bottom
-//     0.1, // near clipping plane
-//     100 // far clipping plane
-//     // we need aspect ratio to make sure that the object is not stretched
-//
-// )
-
-
-// camera.position.x = 2
-// camera.position.y = 2
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
-camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-// controls.target.y = 1
-// controls.update()
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    // mesh.rotation.y = elapsedTime;
-
-    // Update camera
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    // camera.position.y = cursor.y * 5
-    // camera.lookAt(mesh.position)
 
     // Update controls
     controls.update()
