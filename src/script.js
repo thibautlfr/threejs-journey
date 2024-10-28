4bd59fe7efd0c02709403d054263a2207b94f47f
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-10-28 22:33:35 +0100

    [starter]:10-textures

--- src/script.js
+++ src/script.js
@@ -1,30 +1,5 @@
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from 'gsap'
-import GUI from 'lil-gui'
-
-/**
- * Debug
- */
-const gui = new GUI({
-    width: 300,
-    title: 'Nice debug UI',
-    closeFolders: false
-})
-gui.close()
-const debugObject = {}
-
-window.addEventListener('keydown', (event) => {
-    if (event.key === 'h') {
-        gui.show(gui._hidden)
-    }
-})
-
-if(window.location.hash === '#debug') {
-    gui.show()
-} else {
-    gui.hide()
-}
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
@@ -38,51 +13,10 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-debugObject.color = '#427bff'
-
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({
-        color: debugObject.color,
-        wireframe: true
-    }
-)
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
-
-const cubeTweaks = gui.addFolder('Cube')
-// cubeTweaks.close()
-
-cubeTweaks.add(mesh.position, 'y')
-    .min(-3)
-    .max(3)
-    .step(0.01)
-    .name('elevation')
-
-cubeTweaks.add(mesh, 'visible')
-cubeTweaks.add(material, 'wireframe')
-
-cubeTweaks.addColor(debugObject, 'color')
-    .onChange(() => {
-        material.color.set(debugObject.color)
-    })
-
-debugObject.spin = () => {
-    gsap.to(mesh.rotation, {duration: 1, y: mesh.rotation.y + Math.PI * 2})
-}
-
-cubeTweaks.add(debugObject, 'spin')
-
-debugObject.subdivision = 2
-cubeTweaks.add(debugObject, 'subdivision')
-    .min(1)
-    .max(20)
-    .step(1)
-    .onFinishChange(() => {
-        mesh.geometry.dispose()
-        mesh.geometry = new THREE.BoxGeometry(
-            1, 1, 1,
-            debugObject.subdivision, debugObject.subdivision, debugObject.subdivision)
-    })
 
 /**
  * Sizes
@@ -92,7 +26,8 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener('resize', () =>
+{
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -113,7 +48,7 @@ window.addEventListener('resize', () => {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 1
 scene.add(camera)
 
 // Controls
@@ -134,7 +69,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () => {
+const tick = () =>
+{
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
