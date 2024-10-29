ed9b627579aa95488c0d97792436f60c55631e3e
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-10-29 21:20:52 +0100

    [starter]:11-materials

--- src/script.js
+++ src/script.js
@@ -1,111 +1,25 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-/**
- * Textures
- */
-
-const loadingManager = new THREE.LoadingManager()
-// loadingManager.onStart = () => {
-//   console.log("loading started")
-// }
-// loadingManager.onLoad = () => {
-//   console.log("loading finished")
-// }
-// loadingManager.onProgress = () => {
-//   console.log("loading progressing")
-// }
-// loadingManager.onError = () => {
-//   console.log("loading error")
-// }
-
-const textureLoader = new THREE.TextureLoader(loadingManager)
-
-// const colorTexture = textureLoader.load("/textures/door/color.jpg")
-// const colorTexture = textureLoader.load("/textures/checkerboard-1024x1024.png")
-// const colorTexture = textureLoader.load("/textures/checkerboard-8x8.png")
-const colorTexture = textureLoader.load("/textures/minecraft.png")
-const alphaTexture = textureLoader.load("/textures/door/alpha.jpg")
-const heightTexture = textureLoader.load("/textures/door/height.jpg")
-const normalTexture = textureLoader.load("/textures/door/normal.jpg")
-const ambientOcclusionTexture = textureLoader.load("/textures/door/ambientOcclusion.jpg")
-const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg")
-const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg")
-
-colorTexture.colorSpace = THREE.SRGBColorSpace
-alphaTexture.colorSpace = THREE.SRGBColorSpace
-heightTexture.colorSpace = THREE.SRGBColorSpace
-normalTexture.colorSpace = THREE.SRGBColorSpace
-ambientOcclusionTexture.colorSpace = THREE.SRGBColorSpace
-metalnessTexture.colorSpace = THREE.SRGBColorSpace
-roughnessTexture.colorSpace = THREE.SRGBColorSpace
-
-// colorTexture.repeat.x = 2
-// colorTexture.repeat.y = 3
-// colorTexture.wrapS = THREE.RepeatWrapping
-// colorTexture.wrapT = THREE.RepeatWrapping 
-
-// colorTexture.offset.x = 0.5
-// colorTexture.offset.y = 0.5
-
-// colorTexture.rotation = Math.PI * 0.25
-// colorTexture.center.x = 0.5
-// colorTexture.center.y = 0.5
-
-colorTexture.generateMipmaps = false
-colorTexture.minFilter = THREE.NearestFilter
-colorTexture.magFilter = THREE.NearestFilter
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector("canvas.webgl")
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
-// Group
-const group = new THREE.Group()
-scene.add(group)
-
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({
-  map: colorTexture
-})
-const mesh = new THREE.Mesh(geometry, material)
-
-const positions = [
-  { x: -0.5, y: 0 },   // base
-  { x: -1.5, y: 1 },
-  { x: 0.5, y: 1 },
-  { x: -2.5, y: 2 },
-  { x: 1.5, y: 2 },
-  { x: -2.5, y: 3 },
-  { x: 1.5, y: 3 },
-  { x: -1.5, y: 4 },
-  { x: 0.5, y: 4 },
-  { x: -0.5, y: 3 }
-];
-
-positions.forEach(pos => {
-  const clone = mesh.clone();
-  clone.position.set(pos.x, pos.y, 0);
-  group.add(clone);
-});
 
 /**
  * Sizes
  */
 const sizes = {
   width: window.innerWidth,
-  height: window.innerHeight,
+  height: window.innerHeight
 }
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () =>
+{
   // Update sizes
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
@@ -123,15 +37,10 @@ window.addEventListener("resize", () => {
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
-camera.position.y = 3
-camera.position.z = 5
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -142,7 +51,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -152,11 +61,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () => {
+const tick = () =>
+{
   const elapsedTime = clock.getElapsedTime()
-
-  // Update objects
-  group.rotation.y = elapsedTime
 
   // Update controls
   controls.update()
@@ -166,6 +73,6 @@ const tick = () => {
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
-};
+}
 
-tick();
+tick()
