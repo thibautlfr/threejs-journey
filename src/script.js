80cbaaa78b161aa390ddd448f96fa1fb2a769148
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-11-06 21:03:53 +0100

    [starter]:15-shadows

--- src/script.js
+++ src/script.js
@@ -5,7 +5,6 @@ import GUI from "lil-gui";
 /**
  * Base
  */
-
 // Debug
 const gui = new GUI();
 
@@ -18,62 +17,38 @@ const scene = new THREE.Scene();
 /**
  * Lights
  */
-// const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
-// scene.add(ambientLight);
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+gui.add(ambientLight, "intensity").min(0).max(3).step(0.001);
+scene.add(ambientLight);
 
-// const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9);
-// directionalLight.position.set(1, 0.25, 0);
-// scene.add(directionalLight);
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+directionalLight.position.set(2, 2, -1);
+gui.add(directionalLight, "intensity").min(0).max(3).step(0.001);
+gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001);
+scene.add(directionalLight);
 
-// const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.9);
-// scene.add(hemisphereLight);
-
-// const pointLight = new THREE.PointLight(0xffffff, 1.5);
-// pointLight.position.set(2, 3, 4);
-// scene.add(pointLight);
-
-// const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 6, 1, 1);
-// rectAreaLight.position.set(0, 0, 2);
-// scene.add(rectAreaLight);
-
-const spotLight = new THREE.SpotLight(
-  0x78ff00,
-  4.5,
-  10,
-  Math.PI * 0.1,
-  0.25,
-  1
-);
-spotLight.position.set(0, 2, 3);
-scene.add(spotLight);
-
-const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(spotLightHelper);
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.7;
+gui.add(material, "metalness").min(0).max(1).step(0.001);
+gui.add(material, "roughness").min(0).max(1).step(0.001);
 
 /**
  * Objects
  */
-// Material
-const material = new THREE.MeshStandardMaterial();
-material.roughness = 0.4;
-
-// Objects
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
-sphere.position.x = -1.5;
-
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-  material
-);
-torus.position.x = 1.5;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.65;
+plane.position.y = -0.5;
 
-scene.add(sphere, cube, torus, plane);
+scene.add(sphere, plane);
 
 /**
  * Sizes
@@ -132,15 +107,6 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update objects
-  sphere.rotation.y = 0.1 * elapsedTime;
-  cube.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
-
-  sphere.rotation.x = 0.15 * elapsedTime;
-  cube.rotation.x = 0.15 * elapsedTime;
-  torus.rotation.x = 0.15 * elapsedTime;
 
   // Update controls
   controls.update();
