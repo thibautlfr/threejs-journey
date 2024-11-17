2e016dacd85be1fb1b26c342712d6a745b865fd3
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-11-17 17:30:14 +0100

    [starter]:55-first-r3f-application

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
+import react from '@vitejs/plugin-react'
+import { transformWithEsbuild } from 'vite'
 import restart from 'vite-plugin-restart'
 
 export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    root: 'src/',
+    publicDir: '../public/',
+    plugins:
+    [
+        // Restart server on static/public file change
+        restart({ restart: [ '../public/**', ] }),
+
+        // React support
+        react(),
+
+        // .js file support as if it was JSX
+        {
+            name: 'load+transform-js-files-as-jsx',
+            async transform(code, id)
+            {
+                if (!id.match(/src\/.*\.js$/))
+                    return null
+
+                return transformWithEsbuild(code, id, {
+                    loader: 'jsx',
+                    jsx: 'automatic',
+                });
+            },
+        },
+    ],
     server:
     {
         host: true, // Open to local network and display URL
@@ -14,8 +39,4 @@ export default {
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
-    ],
 }
