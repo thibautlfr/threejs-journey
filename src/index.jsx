f6627df6a4cdb4a7168de0e272d7824e1111ab71
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-11-26 00:06:24 +0100

    feat: environment in r3F

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,20 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
+import {Canvas} from '@react-three/fiber'
 import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        camera={ {
+        shadows={false}
+        camera={{
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
+            position: [-4, 3, 6]
+        }}
     >
-        <color args={ ['#ff0000'] } attach="background" />
-        <Experience />
+        <Experience/>
     </Canvas>
 )
