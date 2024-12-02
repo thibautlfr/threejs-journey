247fb078ff7cf7fcc591a6769569092af071bfb9
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-12-02 22:02:19 +0100

    vignette, glitch, noise, bloom

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,31 +1,89 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import {OrbitControls} from '@react-three/drei'
+import {Perf} from 'r3f-perf'
+import {Bloom, EffectComposer, Glitch, Noise, ToneMapping, Vignette} from "@react-three/postprocessing"
+import {ToneMappingMode, BlendFunction, GlitchMode} from "postprocessing"
+import {useControls} from 'leva'
 
-export default function Experience()
-{
+export default function Experience() {
+    const {showVignette, blendFunction, opacity} = useControls('Vignette', {
+        showVignette: {value: false},
+        blendFunction: {value: BlendFunction.NORMAL, options: BlendFunction},
+        opacity: {value: 1, min: 0, max: 1}
+    }, {collapsed: true})
+
+    const {showGlitch, delay, duration, strength, mode} = useControls('Glitch', {
+        showGlitch: {value: false},
+        delay: {value: [0.5, 1], min: 0, max: 2},
+        duration: {value: [0.1, 0.3], min: 0, max: 1},
+        strength: {value: [0.2, 0.4], min: 0, max: 1},
+        mode: {value: GlitchMode.SPORADIC, options: GlitchMode}
+    }, {collapsed: true})
+
+    const {showNoise} = useControls('Noise', {
+        showNoise: {value: false},
+    }, {collapsed: true})
+
     return <>
+        <color args={['#000000']} attach="background"/>
 
-        <Perf position="top-left" />
+        <EffectComposer>
+            <ToneMapping mode={ToneMappingMode.ACES_FILMIC}/>
+            {showVignette &&
+                <Vignette
+                    offset={0.3}
+                    darkness={0.9}
+                    blendFunction={blendFunction}
+                    opacity={opacity}
+                />
+            }
 
-        <OrbitControls makeDefault />
+            {showGlitch &&
+                <Glitch
+                    delay={delay}
+                    duration={duration}
+                    strength={strength}
+                    mode={mode}
+                />
+            }
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+            {showNoise &&
+                <Noise
+                    premultiply
+                    blendFunction={BlendFunction.NORMAL}
+                />
+            }
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
+            <Bloom
+                mipmapBlur
+                luminanceThreshold={0.7}
+                luminanceSmoothing={1}
+                intensity={10}
+            />
+        </EffectComposer>
+
+        <Perf position="top-left"/>
+
+        <OrbitControls makeDefault/>
+
+        <directionalLight castShadow position={[1, 2, 3]} intensity={4.5}/>
+        <ambientLight intensity={1.5}/>
+
+        <mesh castShadow position-x={-2}>
+            <sphereGeometry/>
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+        <mesh castShadow position-x={2} scale={1.5}>
+            <boxGeometry/>
+            <meshBasicMaterial
+                color={[ 1.5 * 10, 1 * 10, 4 * 10]}
+                toneMapped={false}
+            />
         </mesh>
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+        <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+            <planeGeometry/>
+            <meshStandardMaterial color="greenyellow"/>
         </mesh>
-
     </>
 }
