99506df9f3e24542eacc43dbcf6c7a08170a68bd
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-12-03 10:19:15 +0100

    [starter]:65-physics

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,67 +1,31 @@
-import {ContactShadows, Environment, Float, Html, PresentationControls, Text} from '@react-three/drei'
-import {useGLTF} from "@react-three/drei";
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
 
-export default function Experience() {
-    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
-    console.log(computer)
-
+export default function Experience()
+{
     return <>
 
-        <Environment preset={'city'}/>
+        <Perf position="top-left" />
 
-        <color args={["#241a1a"]} attach="background"/>
+        <OrbitControls makeDefault />
 
-        <PresentationControls
-            global
-            rotation={[0.13, 0.1, 0]}
-            polar={[-0.4, 0.2]}
-            azimuth={[-1, 0.75]}
-            config={{mass: 2, tension: 400}}
-            snap={{mass: 4, tension: 400}}
-        >
-            <Float rotationIntensity={0.4}>
-                <rectAreaLight
-                    width={2.5}
-                    height={1.65}
-                    intensity={65}
-                    color={'#6f2fa1'}
-                    rotation={[0.1, Math.PI, 0]}
-                    position={[0, 0.55, -1.15]}
-                />
-                <primitive
-                    object={computer.scene}
-                    position-y={-1.2}
-                >
-                    <Html
-                        transform
-                        wrapperClass="htmlScreen"
-                        distanceFactor={1.17}
-                        position={[0, 1.56, -1.4]}
-                        rotation-x={-0.256}
-                    >
-                        <iframe src="https://www.thibaut-lefrancois.com/"/>
-                    </Html>
-                </primitive>
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
 
-                <Text
-                    font="./bangers-v20-latin-regular.woff"
-                    textAlign={'center'}
-                    fontSize={1}
-                    position={[2, 0.75, 1.25]}
-                    rotation-y={-1.25}
-                    maxWidth={2}
-                >
-                    Thibaut LEFRANCOIS
-                </Text>
-            </Float>
-        </PresentationControls>
+        <mesh castShadow position={ [ - 2, 2, 0 ] }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
 
-        <ContactShadows
-            position-y={-1.4}
-            opacity={0.4}
-            scale={5}
-            blur={2.4}
-        />
+        <mesh castShadow position={ [ 2, 2, 0 ] }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh receiveShadow position-y={ - 1.25 }>
+            <boxGeometry args={ [ 10, 0.5, 10 ] } />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
 
     </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,15 +7,14 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        className="r3f"
+        shadows
         camera={ {
             fov: 45,
             near: 0.1,
-            far: 2000,
-            position: [ -3, 1.5, 6 ]
+            far: 200,
+            position: [ 4, 2, 6 ]
         } }
     >
         <Experience />
-
     </Canvas>
 )
