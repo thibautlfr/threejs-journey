0da414845e66c31fa8e844ca02f531c5726750c3
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2025-03-25 20:48:39 +0100

    [starter]:25-shaders

--- src/script.js
+++ src/script.js
@@ -1,144 +1,98 @@
-import GUI from "lil-gui";
-import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-/**
- * Loaders
- */
-const gltfLoader = new GLTFLoader();
-// const cubeTextureLoader = new THREE.CubeTextureLoader();
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI();
+const gui = new GUI()
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 /**
- * Environment map
+ * Textures
  */
-scene.environmentIntensity = 1;
-scene.backgroundBlurriness = 0;
-scene.backgroundIntensity = 1;
-// scene.backgroundRotation.x = 1;
-// scene.environmentRotation.x = 1;
-
-gui.add(scene, "environmentIntensity").min(0).max(10).step(0.01);
-gui.add(scene, "backgroundBlurriness").min(0).max(1).step(0.01);
-gui.add(scene, "backgroundIntensity").min(0).max(10).step(0.001);
-gui
-  .add(scene.backgroundRotation, "y")
-  .min(0)
-  .max(Math.PI * 2)
-  .step(0.01).name;
-
-//LDR cube texture
-const environmentMap = cubeTextureLoader.load([
-  "/environmentMaps/0/px.png",
-  "/environmentMaps/0/nx.png",
-  "/environmentMaps/0/py.png",
-  "/environmentMaps/0/ny.png",
-  "/environmentMaps/0/pz.png",
-  "/environmentMaps/0/nz.png",
-]);
-
-scene.environment = environmentMap;
-scene.background = environmentMap;
+const textureLoader = new THREE.TextureLoader()
 
 /**
- * Torus Knot
+ * Test mesh
  */
-const torusKnot = new THREE.Mesh(
-  new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
-  new THREE.MeshStandardMaterial({
-    roughness: 0.3,
-    metalness: 1,
-    color: 0xaaaaaa,
-  })
-);
-torusKnot.position.y = 4;
-scene.add(torusKnot);
+// Geometry
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
-/**
- * Models
- */
-gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
-  gltf.scene.scale.set(10, 10, 10);
-  scene.add(gltf.scene);
-});
+// Material
+const material = new THREE.MeshBasicMaterial()
+
+// Mesh
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(4, 5, 4);
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0.25, - 0.25, 1)
+scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.target.y = 3.5;
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-const tick = () => {
-  // Time
-  const elapsedTime = clock.getElapsedTime();
+const clock = new THREE.Clock()
 
-  // Update controls
-  controls.update();
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
-  // Render
-  renderer.render(scene, camera);
+    // Update controls
+    controls.update()
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+    // Render
+    renderer.render(scene, camera)
 
-tick();
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
