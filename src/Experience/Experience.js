d3a8fd654b8af3b8d13dc971b6ceefda7246bed7
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2026-01-13 20:41:40 +0100

    feat: add Camera class and integrate with Experience

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,9 +1,18 @@
 import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/Time.js";
 import * as THREE from "three";
+import Camera from "./Camera.js";
+
+let instance = null
 
 export default class Experience {
   constructor(canvas) {
+    if(instance) {
+      return instance
+    }
+
+    instance = this
+
     // Global access
     window.experience = this
 
@@ -14,6 +23,7 @@ export default class Experience {
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
+    this.camera = new Camera()
 
     this.sizes.on('resize', () => {
       this.resize()
@@ -25,8 +35,10 @@ export default class Experience {
   }
 
   resize() {
+    this.camera.resize()
   }
 
   update() {
+    this.camera.update()
   }
 }
--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
-import Robot from "./Robot.js";
-import FlyingRobot from "./FlyingRobot.js";
+import './style.css'
+import Experience from "./Experience/Experience.js";
 
-const wallE = new Robot('Wall-E', 0);
-const ultron = new FlyingRobot('Ultron', 2);
-const astroBoy = new FlyingRobot('AstroBoy', 2);
+const experience = new Experience(document.querySelector('canvas.webgl'));
+
 
 // import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
