8a68b0a645a11d61230695aba5fab83426ecd161
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2026-01-13 21:27:35 +0100

    feat: add Resources class for asset management and integrate with Experience and World

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -5,14 +5,17 @@ import Environment from "./Environment.js";
 export default class World {
   constructor() {
     this.experience = new Experience()
+    this.scene = this.experience.scene
+    this.resources = this.experience.resources
 
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({wireframe: true})
+      new THREE.MeshBasicMaterial({color: 0xffffff}),
     )
-    this.experience.scene.add(testMesh)
+    this.scene.add(testMesh)
 
-    // Setup
-    this.environment = new Environment()
+    this.resources.on('ready', () => {
+      this.environment = new Environment()
+    })
   }
 }
