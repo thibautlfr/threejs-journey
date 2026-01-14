4f8bfa46f6bfb49d0f7f3a4a080bb330bb4e9323
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2026-01-14 20:02:17 +0100

    feat: add destroy method to Experience class for proper cleanup

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -53,4 +53,33 @@ export default class Experience {
     this.world.update()
     this.renderer.update()
   }
+
+  destroy() {
+    this.sizes.off('resize')
+    this.time.off('tick')
+    this.resources.off('ready')
+    window.removeEventListener('resize', this.resize)
+
+    this.scene.traverse((child) => {
+      if(child instanceof THREE.Mesh) {
+        child.geometry.dispose()
+
+        for(const key in child.material) {
+          const value = child.material[key]
+          if(value && typeof value.dispose === 'function') {
+            value.dispose()
+          }
+        }
+      }
+    })
+
+    this.camera.controls.dispose()
+    this.renderer.instance.dispose()
+
+    if(this.debug.active) {
+      this.debug.ui.destroy()
+    }
+
+    console.log('Experience destroyed')
+  }
 }
