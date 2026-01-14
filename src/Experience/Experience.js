8d6fe9a9fb0681bf885fb57d9d12f74f94e6019a
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2026-01-14 19:39:10 +0100

    feat: add debugging functionality with dat.gui for Environment and Fox classes

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -6,6 +6,7 @@ import Renderer from "./Renderer.js";
 import World from "./World/World.js";
 import Resources from "./Utils/Resources.js";
 import sources from "./sources.js";
+import Debug from "./Utils/Debug.js";
 
 let instance = null
 
@@ -24,6 +25,7 @@ export default class Experience {
     this.canvas = canvas
 
     // Setup
+    this.debug = new Debug()
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
