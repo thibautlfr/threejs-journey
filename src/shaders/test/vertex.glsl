a58bfaee228f35c00a77fe79847658fa828d382c
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2026-01-19 22:51:33 +0100

    feat: adjust vertex shader to enhance model position calculation like waving

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -8,6 +8,8 @@ void main() {
   // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
   
   vec4 modelPosition = modelMatrix * vec4(position, 1.0); 
+  modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+  
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
