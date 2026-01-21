fc988c682870612d7fed7d1b2c2c378912d27374
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2026-01-21 19:44:39 +0100

    feat: enhance fragment and vertex shaders with elevation calculations for dynamic effects

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -2,9 +2,13 @@ precision mediump float;
 
 uniform vec3 uColor; // our custom uniform
 uniform sampler2D uTexture; // our custom uniform
+
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
   vec4 textureColor = texture2D(uTexture, vUv);
+  textureColor.rgb *= vElevation * 2.0 + 0.8;
+
   gl_FragColor = textureColor;
 }
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -8,17 +8,21 @@ attribute vec3 position;
 attribute vec2 uv;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
-  // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-  
   vec4 modelPosition = modelMatrix * vec4(position, 1.0); 
-  modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1; // modify the position based on the custom attribute
-  modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1; // modify the position based on the custom attribute
+
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+  modelPosition.z += elevation;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
+
+  // Varyings
   vUv = uv;
+  vElevation = elevation;
 }
