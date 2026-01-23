d9d48fe250460672aa1f4e31bf3f4ca19a0ec33a
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2026-01-23 14:49:28 +0100

    feat: add waves to our plane with vertex shader

---
+++ src/shaders/water/vertex.glsl
@@ -0,0 +1,23 @@
+uniform float uTime;
+
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
+
+void main() {
+  float speed = uTime * uBigWavesSpeed;
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+  // Elevation
+  float xElevation = sin(modelPosition.x * uBigWavesFrequency.x + speed);
+  float zElevation = sin(modelPosition.z * uBigWavesFrequency.y + speed);
+
+  float elevation = xElevation * zElevation * uBigWavesElevation;
+    
+  modelPosition.y += elevation;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+}
