549cdb4b983c07b0649a13ff5aa66c787f5a37ae
thibautlfr <thibaut.lfr@gmail.com>
2026-06-18 00:23:06 +0200

    [starter]:31-modified-materials

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import restart from 'vite-plugin-restart'
-import glsl from 'vite-plugin-glsl'
 
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
+    root: 'src/', // Sources files (typically where index.html is)
+    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
     {
         host: true, // Open to local network and display URL
@@ -18,7 +16,6 @@ export default {
     },
     plugins:
     [
-        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
-        glsl() // Handle shader files
-    ]
+        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    ],
 }
