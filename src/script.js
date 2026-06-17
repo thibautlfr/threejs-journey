a786eddbd13aa48b3ac6da94adda64adfb8eb154
thibautlfr <thibaut.lfr@gmail.com>
2026-06-18 00:55:05 +0200

    feat: add time-based rotation to vertex shader for dynamic effects

--- src/script.js
+++ src/script.js
@@ -69,6 +69,40 @@ const material = new THREE.MeshStandardMaterial( {
     normalMap: normalTexture
 })
 
+const customUniforms = {
+    uTime: { value: 0 }
+}
+
+material.onBeforeCompile = (shader) => {
+    shader.uniforms.uTime = customUniforms.uTime
+
+    shader.vertexShader = shader.vertexShader.replace(
+      '#include <begin_vertex>',
+      `
+        #include <begin_vertex>
+        
+        float angle = (position.y + uTime) * 0.9;
+        mat2 rotateMatrix = get2dRotateMatrix(angle);
+        
+        transformed.xz = rotateMatrix * transformed.xz;
+      `
+    )
+
+    shader.vertexShader = shader.vertexShader.replace(
+      '#include <common>',
+      `
+        #include <common>
+        
+        uniform float uTime;
+        
+        mat2 get2dRotateMatrix(float _angle)
+        {
+            return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+        }
+      `
+    )
+}
+
 /**
  * Models
  */
@@ -155,6 +189,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update custom uniforms
+    customUniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update()
