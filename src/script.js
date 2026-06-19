c95688f7f1168affbaf9a8d99d523e26de9ce505
thibautlfr <thibaut.lfr@gmail.com>
2026-06-19 10:24:17 +0200

    feat: add firework effect with vertex and fragment shaders

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import fireworkVertexShader from './shaders/firework/vertex.glsl'
+import fireworkFragmentShader from './shaders/firework/fragment.glsl'
 
 /**
  * Base
@@ -22,14 +24,18 @@ const textureLoader = new THREE.TextureLoader()
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2),
 }
+sizes.resolution = new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
 window.addEventListener('resize', () =>
 {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
+    sizes.resolution.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -37,7 +43,7 @@ window.addEventListener('resize', () =>
 
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setPixelRatio(sizes.pixelRatio)
 })
 
 /**
@@ -60,16 +66,83 @@ const renderer = new THREE.WebGLRenderer({
     antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(sizes.pixelRatio)
 
 /**
- * Test
+ * Fireworks
  */
-const test = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+const textures = [
+  textureLoader.load('./particles/1.png'),
+  textureLoader.load('./particles/2.png'),
+  textureLoader.load('./particles/3.png'),
+  textureLoader.load('./particles/4.png'),
+  textureLoader.load('./particles/5.png'),
+  textureLoader.load('./particles/6.png'),
+  textureLoader.load('./particles/7.png'),
+  textureLoader.load('./particles/8.png'),
+]
+
+console.log(textures)
+
+const createFirework = (count, position, size, texture, radius, color) => {
+    // Geometry
+    const positionsArray = new Float32Array(count * 3)
+    const sizesArray = new Float32Array(count)
+
+    for (let i = 0 ; i < count ; i++) {
+        const i3 = i * 3
+
+        const spherical = new THREE.Spherical(
+          radius * (0.75 + Math.random() * 0.25),
+          Math.random() * Math.PI,
+          Math.random() * Math.PI * 2,
+        )
+
+        const position = new THREE.Vector3()
+        position.setFromSpherical(spherical)
+
+        positionsArray[i3    ] = position.x
+        positionsArray[i3 + 1] = position.y
+        positionsArray[i3 + 2] = position.z
+
+        sizesArray[i] = Math.random()
+    }
+
+    // Geometry
+    const geometry = new THREE.BufferGeometry()
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positionsArray, 3))
+    geometry.setAttribute('aSize', new THREE.Float32BufferAttribute(sizesArray, 1))
+
+    // Material
+    texture.flipY = false;
+    const material = new THREE.ShaderMaterial({
+        vertexShader: fireworkVertexShader,
+        fragmentShader: fireworkFragmentShader,
+        transparent: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        uniforms: {
+            uSize: new THREE.Uniform(size),
+            uResolution: new THREE.Uniform(sizes.resolution),
+            uTexture: new THREE.Uniform(texture),
+            uColor: new THREE.Uniform(color),
+        }
+    })
+
+    // Points
+    const firework = new THREE.Points(geometry, material)
+    firework.position.copy(position)
+    scene.add(firework)
+}
+
+createFirework(
+  100,
+  new THREE.Vector3(),
+  0.5,
+  textures[7],
+  1,
+  new THREE.Color('#8affff')
 )
-scene.add(test)
 
 /**
  * Animate
---
+++ src/shaders/firework/vertex.glsl
@@ -0,0 +1,16 @@
+uniform float uSize;
+uniform vec2 uResolution;
+
+attribute float aSize;
+
+void main () {
+
+    // Final position
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    gl_Position = projectionMatrix * viewPosition;
+
+    // Final point size
+    gl_PointSize = uSize * uResolution.y * aSize;
+    gl_PointSize *= 1.0 / - viewPosition.z;
+}
