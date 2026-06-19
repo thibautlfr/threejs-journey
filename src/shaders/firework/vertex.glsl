ced044e4c10b7bb2b593f1815c534a6adbd4d7e7
thibautlfr <thibaut.lfr@gmail.com>
2026-06-19 10:50:41 +0200

    feat: add size scaling to vertex shader for dynamic point size adjustment

--- src/shaders/firework/vertex.glsl
+++ src/shaders/firework/vertex.glsl
@@ -24,12 +24,18 @@ void main () {
     fallingProgress = 1.0 - pow(1.0 - fallingProgress, 3.0);
     newPosition.y -= fallingProgress * 0.2;
 
+    // Scaling
+    float sizeOpeningProgress = remap(uProgress, 0.0, 0.125, 0.0, 1.0);
+    float sizeClosingProgress = remap(uProgress, 0.125, 1.0, 1.0, 0.0);
+    float sizeProgress = min(sizeOpeningProgress, sizeClosingProgress);
+    sizeProgress = clamp(sizeProgress, 0.0, 1.0);
+
     // Final position
     vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
 
     // Final point size
-    gl_PointSize = uSize * uResolution.y * aSize;
+    gl_PointSize = uSize * uResolution.y * aSize * sizeProgress;
     gl_PointSize *= 1.0 / - viewPosition.z;
 }
