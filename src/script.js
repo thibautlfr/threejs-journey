53d2180642f0fd7332c1a6f8116ffaf29f9007f2
thibautlfr <thibaut.lfr@gmail.com>
2026-07-03 19:17:44 +0200

    feat: enhance lighting system with point and directional light helpers

--- src/script.js
+++ src/script.js
@@ -129,6 +129,37 @@ gltfLoader.load(
 )
 
 /**
+ * Light helpers
+ */
+
+// Directional Light Helper
+const directionalLightHelper = new THREE.Mesh(
+  new THREE.PlaneGeometry(),
+  new THREE.MeshBasicMaterial()
+)
+directionalLightHelper.material.color.setRGB(0.1, 0.1, 1)
+directionalLightHelper.material.side = THREE.DoubleSide
+directionalLightHelper.position.set(0, 0, 3)
+scene.add(directionalLightHelper)
+
+// Point Lights Helper
+const pointLightHelper = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+pointLightHelper.material.color.setRGB(1.0, 0.1, 0.1)
+pointLightHelper.position.set(0, 2.5, 0)
+scene.add(pointLightHelper)
+
+const pointLightHelper2 = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+pointLightHelper2.material.color.setRGB(0.1, 1.0, 0.5)
+pointLightHelper2.position.set(2, 2, 2)
+scene.add(pointLightHelper2)
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
