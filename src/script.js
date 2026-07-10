53ece7aae7ef073166ab7e12d092fc5f3a725781
thibautlfr <thibaut.lfr@gmail.com>
2026-07-10 15:36:53 +0200

    feat: add halftone shading effect and enhance material properties in fragment shader

--- src/script.js
+++ src/script.js
@@ -36,6 +36,12 @@ window.addEventListener('resize', () =>
     sizes.height = window.innerHeight
     sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
+    // Update materials
+    material.uniforms.uResolution.value.set(
+      sizes.width / sizes.pixelRatio,
+      sizes.height / sizes.pixelRatio,
+    )
+
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
@@ -85,6 +91,8 @@ gui
  */
 const materialParameters = {}
 materialParameters.color = '#ff794d'
+materialParameters.shadowColor = '#8e19b8'
+materialParameters.lightColor = '#e5ffe0'
 
 const material = new THREE.ShaderMaterial({
     vertexShader: halftoneVertexShader,
@@ -93,6 +101,12 @@ const material = new THREE.ShaderMaterial({
     {
         uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
         uShadeColor: new THREE.Uniform(new THREE.Color(materialParameters.shadeColor)),
+        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+        uShadowRepetitions: new THREE.Uniform(100),
+        uLightRepetitions: new THREE.Uniform(130),
+        uShadowColor: new THREE.Uniform(new THREE.Color(materialParameters.shadowColor)),
+        uLightColor: new THREE.Uniform(new THREE.Color(materialParameters.lightColor)),
+
     }
 })
 
@@ -102,6 +116,26 @@ gui
     {
         material.uniforms.uColor.value.set(materialParameters.color)
     })
+
+gui.add(
+  material.uniforms.uShadowRepetitions, 'value'
+).min(1).max(300).step(1)
+
+gui.add(
+  material.uniforms.uLightRepetitions, 'value'
+).min(1).max(300).step(1)
+
+gui.addColor(
+  materialParameters, 'shadowColor'
+).onChange(() => {
+    material.uniforms.uShadowColor.value.set(materialParameters.shadowColor)
+})
+
+gui.addColor(
+  materialParameters, 'lightColor'
+).onChange(() => {
+    material.uniforms.uLightColor.value.set(materialParameters.lightColor)
+})
 
 /**
  * Objects
