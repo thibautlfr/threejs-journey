5c789a6b065e6b12a7422d51f021533c3010c117
thibautlfr <thibaut.lfr@gmail.com>
2026-07-13 15:33:38 +0200

    feat: add sun direction uniform and implement sun update logic in scene

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -5,6 +5,7 @@ varying vec3 vPosition;
 uniform sampler2D uDayTexture;
 uniform sampler2D uNightTexture;
 uniform sampler2D uSpecularCloudsTexture;
+uniform vec3 uSunDirection;
 
 void main()
 {
@@ -13,7 +14,6 @@ void main()
     vec3 color = vec3(0.0);
 
     // Sun orientation
-    vec3 uSunDirection = vec3(0.0, 0.0, 1.0);
     float sunOrientation = dot(uSunDirection, normal);
 
     // Day / night color
