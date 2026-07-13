63114b61af21b700364f943fa059431baaecc28b
thibautlfr <thibaut.lfr@gmail.com>
2026-07-13 15:19:12 +0200

    feat: implement day/night color blending in fragment shader and load textures in script

--- src/script.js
+++ src/script.js
@@ -22,6 +22,16 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * Earth
  */
+
+// Textures
+const earthDayTexture = textureLoader.load('./earth/day.jpg')
+earthDayTexture.colorSpace = THREE.SRGBColorSpace
+
+const earthNightTexture = textureLoader.load('./earth/night.jpg')
+earthNightTexture.colorSpace = THREE.SRGBColorSpace
+
+const earthSpecularCloudsTexture = textureLoader.load('./earth/specularClouds.jpg')
+
 // Mesh
 const earthGeometry = new THREE.SphereGeometry(2, 64, 64)
 const earthMaterial = new THREE.ShaderMaterial({
@@ -29,6 +39,9 @@ const earthMaterial = new THREE.ShaderMaterial({
     fragmentShader: earthFragmentShader,
     uniforms:
     {
+        uDayTexture: new THREE.Uniform(earthDayTexture),
+        uNightTexture: new THREE.Uniform(earthNightTexture),
+        uSpecularCloudsTexture: new THREE.Uniform(earthSpecularCloudsTexture)
     }
 })
 const earth = new THREE.Mesh(earthGeometry, earthMaterial)
--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -2,11 +2,25 @@ varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularCloudsTexture;
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // Sun orientation
+    vec3 uSunDirection = vec3(0.0, 0.0, 1.0);
+    float sunOrientation = dot(uSunDirection, normal);
+
+    // Day / night color
+    float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
+    vec3 dayColor = texture2D(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture2D(uNightTexture, vUv).rgb;
+    color = mix(nightColor, dayColor, dayMix);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
